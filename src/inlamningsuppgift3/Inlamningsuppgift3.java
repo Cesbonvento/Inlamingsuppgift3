7135e77955fe2c5c2a572e37697549e58cae76d3
Fidél Diaz <fidel.diaz@yh.nackademin.se>
2018-10-26 14:47:57 +0200

    Feature branch

--- src/inlamningsuppgift3/Inlamningsuppgift3.java
+++ src/inlamningsuppgift3/Inlamningsuppgift3.java
@@ -3,15 +3,23 @@
  */
 package inlamningsuppgift3;
 
+import javax.swing.*;
+import java.awt.*;
 /**
  *
  * @author Bazgir
  */
 public class Inlamningsuppgift3 {
 
- 
+    JButton button = new JButton("uno"); 
+    Inlamningsuppgift3()
+    {
+         
+    }
+    
     public static void main(String[] args) {
-        //Feature branch
+        //Feature branch 
+        Inlamningsuppgift3 uppgift3 = new Inlamningsuppgift3();
     }
     
 }
