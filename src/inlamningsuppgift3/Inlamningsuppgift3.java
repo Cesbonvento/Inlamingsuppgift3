77b1843b48c9f9739b55b4170db0380cd86bf58a
Fidél Diaz <fidel.diaz@yh.nackademin.se>
2018-10-28 12:55:37 +0100

    Feature branch

--- src/inlamningsuppgift3/Inlamningsuppgift3.java
+++ src/inlamningsuppgift3/Inlamningsuppgift3.java
@@ -13,21 +13,20 @@ public class Inlamningsuppgift3 {
 
     JButton[] button = new JButton[16];
     JButton nyttSpel = new JButton("Nytt spel"); 
-    JLabel spel = new JLabel("Välkommen till 15-spel!");
+    JPanel jp = new JPanel();
     Inlamningsuppgift3()
     {
-        JFrame jrf = new JFrame("This is a 15-puzzle game"); 
-        jrf.setLayout(new GridLayout(4,4));
-        //jrf.add(spel);
-        
+        JFrame jrf = new JFrame("This is a 15-puzzle game");  
+        jrf.setLayout(new FlowLayout(1));   
+        jrf.add(jp);
+        //jrf.add(nyttSpel);
+        jp.setLayout(new GridLayout(4,4));
         for (int i=1; i<16; i++){
             button[i] = new JButton(String.valueOf(i)); 
-            jrf.add(button[i]);
+            jp.add(button[i]);
         }
-        jrf.add(button[0] = new JButton(" ")); 
-        
-       
-        
+        jp.add(button[0] = new JButton(" "));  
+        jrf.add(nyttSpel);  
         jrf.setLocation(100, 100); 
         jrf.setSize(800, 600); 
         jrf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
