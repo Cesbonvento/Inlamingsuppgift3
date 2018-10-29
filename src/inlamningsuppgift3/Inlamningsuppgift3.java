4c9615365846e5cab74dcef29121072df35b8f6f
Fidél Diaz <fidel.diaz@yh.nackademin.se>
2018-10-29 14:40:55 +0100

    Feature branch

--- src/inlamningsuppgift3/Inlamningsuppgift3.java
+++ src/inlamningsuppgift3/Inlamningsuppgift3.java
@@ -11,26 +11,27 @@ import java.awt.*;
  */
 public class Inlamningsuppgift3 {
 
+    JFrame jrf = new JFrame("This is a 15-puzzle game");  
     JButton[] button = new JButton[16];
     JButton nyttSpel = new JButton("Nytt spel"); 
-    JPanel jp = new JPanel();
+    JPanel panel1 = new JPanel(); 
+    JPanel panel2 = new JPanel();
     Inlamningsuppgift3()
-    {
-        JFrame jrf = new JFrame("This is a 15-puzzle game");  
-        jrf.setLayout(new FlowLayout(1));   
-        jrf.add(jp);
-        //jrf.add(nyttSpel);
-        jp.setLayout(new GridLayout(4,4));
-        for (int i=1; i<16; i++){
-            button[i] = new JButton(String.valueOf(i)); 
-            jp.add(button[i]);
-        }
-        jp.add(button[0] = new JButton(" "));  
-        jrf.add(nyttSpel);  
+    { 
+        jrf.add(panel1, BorderLayout.CENTER);
+        jrf.add(panel2, BorderLayout.SOUTH);
+        panel1.setLayout(new GridLayout(4,4));  
+        panel2.add(nyttSpel);  
         jrf.setLocation(100, 100); 
         jrf.setSize(800, 600); 
         jrf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jrf.setVisible(true);
+        jrf.setVisible(true); 
+        
+        for (int i=1; i<16; i++){
+            button[i] = new JButton(String.valueOf(i)); 
+            panel1.add(button[i]);
+        } 
+        panel1.add(button[0] = new JButton(" "));
         
         
     }
