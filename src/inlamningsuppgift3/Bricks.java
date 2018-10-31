5f0340ecbe092e2202999ecfc0e93bfd1fa1ed49
Fidél Diaz <fidel.diaz@yh.nackademin.se>
2018-10-31 12:48:05 +0100

    Feature branch

---
+++ src/inlamningsuppgift3/Bricks.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inlamningsuppgift3;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Bazgir
+ */
+public class Bricks extends JPanel implements ActionListener {
+
+    JButton[] button = new JButton[16];
+    JButton nyttSpel = new JButton("Nytt spel"); 
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
