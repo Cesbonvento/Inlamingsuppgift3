ae70aa576a8a00e29e0921e9639f19f0847702c2
Fidél Diaz <fidel.diaz@yh.nackademin.se>
2018-10-31 13:30:02 +0100

    Feature branch

--- src/inlamningsuppgift3/Inlamningsuppgift3.java
+++ src/inlamningsuppgift3/Inlamningsuppgift3.java
@@ -5,40 +5,44 @@ package inlamningsuppgift3;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 /**
  *
  * @author Bazgir
  */
-public class Inlamningsuppgift3 {
+public class Inlamningsuppgift3 extends JFrame implements ActionListener{
 
-    JFrame jrf = new JFrame("This is a 15-puzzle game");  
-    JButton[] button = new JButton[16];
-    JButton nyttSpel = new JButton("Nytt spel"); 
+    JFrame jrf = new JFrame("This is a 15-puzzle game");   
+    Bricks puzzle = new Bricks();
+    JButton newGame = new JButton("Nytt spel"); 
     JPanel panel1 = new JPanel(); 
     JPanel panel2 = new JPanel();
     Inlamningsuppgift3()
     { 
         jrf.add(panel1, BorderLayout.CENTER);
         jrf.add(panel2, BorderLayout.SOUTH);
-        panel1.setLayout(new GridLayout(4,4));  
-        panel2.add(nyttSpel);  
+        jrf.setLayout(new FlowLayout()); 
+        
+        panel1.add(puzzle); 
+        panel2.add(newGame);
+        
         jrf.setLocation(100, 100); 
-        jrf.setSize(800, 600); 
+        jrf.setSize(500, 500); 
         jrf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jrf.setVisible(true); 
         
-        for (int i=1; i<16; i++){
-            button[i] = new JButton(String.valueOf(i)); 
-            panel1.add(button[i]);
-        } 
-        panel1.add(button[0] = new JButton(" "));
-        
-        
-    }
+    } 
+    
     
     public static void main(String[] args) {
         //Feature branch 
         Inlamningsuppgift3 uppgift3 = new Inlamningsuppgift3(); 
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
