a0ae7b44bae5cac3be9c6eed992df85955e6368d
Fidél Diaz <fidel.diaz@yh.nackademin.se>
2018-10-31 13:30:18 +0100

    Feature branch

--- src/inlamningsuppgift3/Bricks.java
+++ src/inlamningsuppgift3/Bricks.java
@@ -5,22 +5,45 @@
  */
 package inlamningsuppgift3;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.*;
-import javax.swing.*;
+import javax.swing.*; 
+import java.util.*;
 
 /**
  *
  * @author Bazgir
  */
-public class Bricks extends JPanel implements ActionListener {
+public class Bricks extends JPanel implements ActionListener 
+{
 
-    JButton[] button = new JButton[16];
-    JButton nyttSpel = new JButton("Nytt spel"); 
-    
-    
+    List<JButton> bricks;
+    JButton brick; 
+    JPanel board;
+    public Bricks()
+    { 
+        board.setLayout(new GridLayout(4,4)); 
+        
+        bricks = new ArrayList<>(16);        //Ska endast innehålla setxon knappar etc. 
+        for (int i=1; i<16; i++){
+            brick = new JButton(String.valueOf(i)); 
+           // brick.setVisible(true);
+            brick.addActionListener(this); 
+            bricks.add(brick); 
+        } 
+        
+        JButton blankBrick = new JButton(" "); 
+        //blankBrick.setVisible(true);
+        blankBrick.addActionListener(this);
+        bricks.add(blankBrick); 
+    }  
+
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+        
+
     
 }
