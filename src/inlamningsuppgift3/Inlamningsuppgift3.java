72cb244e9ba75b32b63738cf72c4d30258d3ac29
Fidél Diaz <fidel.diaz@yh.nackademin.se>
2018-11-02 01:29:59 +0100

    Checkerino med allt. Ska bara kommentera och förmodligen kolla om jag kan göra lite ändringar.

--- src/inlamningsuppgift3/Inlamningsuppgift3.java
+++ src/inlamningsuppgift3/Inlamningsuppgift3.java
@@ -11,38 +11,32 @@ import java.awt.event.ActionListener;
  *
  * @author Bazgir
  */
-public class Inlamningsuppgift3 extends JFrame implements ActionListener{
+public class Inlamningsuppgift3 extends JFrame{
 
     JFrame jrf = new JFrame("This is a 15-puzzle game");   
     Bricks puzzle = new Bricks();
     JButton newGame = new JButton("Nytt spel"); 
-    JPanel panel1 = new JPanel(); 
-    JPanel panel2 = new JPanel();
     Inlamningsuppgift3()
     { 
-        jrf.add(panel1, BorderLayout.CENTER);
-        jrf.add(panel2, BorderLayout.SOUTH);
-        jrf.setLayout(new FlowLayout()); 
+        jrf.setLayout(new BorderLayout()); 
+        jrf.add(puzzle);
+        jrf.add(newGame, BorderLayout.SOUTH); 
         
-        panel1.add(puzzle); 
-        panel2.add(newGame);
-        
+        jrf.pack();
         jrf.setLocation(100, 100); 
-        jrf.setSize(500, 500); 
+        jrf.setSize(700, 700); 
         jrf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jrf.setVisible(true); 
+    
+        newGame.addActionListener((ActionEvent ae) -> { 
+            puzzle.newGame();
+         });
+    }  
         
-    } 
-    
-    
     public static void main(String[] args) {
         //Feature branch 
         Inlamningsuppgift3 uppgift3 = new Inlamningsuppgift3(); 
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
 }
